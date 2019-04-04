058163e59a63bb96a49f127536105450321b78e2
Bigo Mendoza <bigo@thejonasproject.org>
2019-04-04 10:14:04 -0700

    Added options in content page to display FB footer link to feed

--- includes/facebook-footer-link-content.php
+++ includes/facebook-footer-link-content.php
@@ -2,12 +2,20 @@
 
 function ffl_add_footer($content){ // the $content is the entire article of the blog
 
+	global $ffl_options;
 	$footer_output = '<hr>';
 	$footer_output .= '<div class="footer_content">';
 	$footer_output .= '<span class="dashicons dashicons-facebook"></span>Find me on <a target="_blank" href="http://www.facebook.com/oscar">Facebook Link</a>';
 	$footer_output .='</div>';
 
+if(is_home() || $ffl_options['enable']){
+			return $content . $footer_output;
+			}else{
+			return $content;		
+		}
+
 	return $content . $footer_output;
+
 
 }
 
